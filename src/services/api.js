f97f97f227b8132f13968fb41e935f4959b7a3b1
Mateus <mateus3u@gmail.com>
2020-03-09 09:34:56 -0300

    corrigindo url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://www.apimateus.life/gobarber/',
+  baseURL: 'https://www.apimateus.life/gobarber',
 });
 
 export default api;
